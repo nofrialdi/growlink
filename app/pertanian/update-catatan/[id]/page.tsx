c8d2c292539f903ada2c0e06a65a8d28f3da0722
Agus Siswanto <xcodeme21@gmail.com>
2023-11-25 01:23:05 +0700

    update catatan

--- app/pertanian/update-catatan/[id]/page.tsx
+++ app/pertanian/update-catatan/[id]/page.tsx
@@ -26,278 +26,368 @@ import { Dayjs } from "dayjs";
 import Navigation from "@/app/components/navigation2";
 import Shortcut from "@/app/components/shortcut";
 import { format } from "path";
+import { updateDataPertanian } from "@/services/transactions-service";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface Product {
-	id: number;
-	name: string;
+  id: number;
+  name: string;
 }
 
 interface Yields {
-	id: number;
-	productId: number;
-	product: string;
-	plantingTime: string;
-	harvestTime: string;
-	description: string;
-	quantity: number;
+  id: number;
+  productId: number;
+  product: string;
+  plantingTime: string;
+  harvestTime: string;
+  description: string;
+  quantity: number;
 }
 
-export default function UpdatePertanian({ params }: { params: { id: string } }) {
-	const router = useRouter();
+export default function UpdatePertanian({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const router = useRouter();
 
-	const [selectedProduct, setSelectedProduct] = useState("");
-	const [products, setProducts] = useState<Product[]>([]);
-	const [plantingTime, setPlantingTime] = useState("");
-	const [selectedPlantingTime, setSelectedPlantingTime] = useState(new Date(plantingTime));
-	const [selectedHarvestTime, setSelectedHarvestTime] = useState("");
-	const [harvestTime, setHarvestTime] = useState("");
-	const [description, setDescription] = useState("");
-	const [quantity, setQuantity] = useState(0);
-	const [yields, setYields] = useState<Yields[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
+  const [plantingTime, setPlantingTime] = useState("");
+  const [selectedPlantingTime, setSelectedPlantingTime] = useState(
+    new Date(plantingTime)
+  );
+  const [selectedHarvestTime, setSelectedHarvestTime] = useState("");
+  const [harvestTime, setHarvestTime] = useState("");
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [yields, setYields] = useState<Yields[]>([]);
+  const [selectedPlantingDate, setSelectedPlantingDate] = useState(null);
+  const [selectedHarvestDate, setSelectedHarvestDate] = useState(null);
 
-	const handleChangePlantingTime = (date: any) => {
-		setSelectedPlantingTime(date);
-	};
+  const handleHarvestDateChange = (date: any) => {
+    setSelectedHarvestDate(date);
+  };
 
-	async function getProducts() {
-		try {
-			const response = await fetch(`${process.env.NEXT_PUBLIC_SERVICE_BASE}/yields/products`, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			const data = await response.json();
-			setProducts(data);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  const handlePlantingDateChange = (date: any) => {
+    setSelectedPlantingDate(date);
+  };
 
-	const handleChange = (event: SelectChangeEvent) => {
-		setSelectedProduct(event.target.value as string);
-	};
+  async function getProducts() {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVICE_BASE}/yields/products`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	async function getYields() {
-		try {
-			const response = await fetch(`${process.env.NEXT_PUBLIC_SERVICE_BASE}/yields/${params.id}`, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-				},
-			});
-			const data = await response.json();
-			setPlantingTime(data.plantingTime);
-			setHarvestTime(data.harvestTime);
-			setDescription(data.description);
-			setQuantity(data.quantity);
-			console.log(data);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelectedProduct(event.target.value as string);
+  };
 
-	useEffect(() => {
-		getProducts();
-		getYields();
-	}, []);
+  async function getYields() {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVICE_BASE}/yields/${params.id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      const data = await response.json();
+      setPlantingTime(data.plantingTime);
+      setHarvestTime(data.harvestTime);
+      setDescription(data.description);
+      setQuantity(data.quantity);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	var isoDateplantsString = plantingTime;
-	const isoDatePlats = new Date(isoDateplantsString);
-	const options = { year: "numeric", month: "numeric", day: "numeric" };
-	const formattedDatePlants = isoDatePlats.toLocaleDateString(
-		"id-ID",
-		options as Intl.DateTimeFormatOptions
-	);
+  useEffect(() => {
+    getProducts();
+    getYields();
+  }, []);
 
-	console.log(formattedDatePlants);
+  var isoDateplantsString = plantingTime;
+  const isoDatePlats = new Date(isoDateplantsString);
+  const options = { year: "numeric", month: "numeric", day: "numeric" };
+  const formattedDatePlants = isoDatePlats.toLocaleDateString(
+    "id-ID",
+    options as Intl.DateTimeFormatOptions
+  );
 
-	return (
-		// <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-		// 	<Box
-		// 		sx={{
-		// 			bgcolor: "primary.main",
-		// 			height: 45,
-		// 			width: "100%",
-		// 			alignItems: "center",
-		// 			p: 0,
-		// 			boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-		// 			zIndex: 2,
-		// 			position: "fixed",
-		// 		}}
-		// 	>
-		// 		<Button
-		// 			variant="text"
-		// 			startIcon={<ArrowBackIosNewIcon />}
-		// 			sx={{ bgcolor: "primary.main", color: "white" }}
-		// 			onClick={() => router.push("/home")}
-		// 		>
-		// 			Catat
-		// 		</Button>
-		// 	</Box>
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data: any = {};
 
-		// 	<Box
-		// 		sx={{
-		// 			mt: 8,
-		// 			width: 320,
-		// 			display: "flex",
-		// 			flexDirection: "column",
-		// 			alignItems: "center",
-		// 		}}
-		// 	>
-		// 		<Typography variant="h6" component="h6" sx={{ textAlign: "center", color: "primary.main" }}>
-		// 			Ubah Data Pertanian
-		// 		</Typography>
+    Array.from(formData.entries()).forEach(([key, value]) => {
+      data[key] = value;
+    });
 
-		// 		<Box
-		// 			component="form"
-		// 			sx={{
-		// 				"& .MuiTextField-root": { width: 320, display: "flex", flexDirection: "column" },
-		// 			}}
-		// 			noValidate
-		// 			autoComplete="off"
-		// 		>
-		// 			<TextField required sx={{ mt: 3 }} fullWidth label="Produk" id="produk" type="text" />
+    data.plantingTime = selectedPlantingDate;
+    data.harvestTime = selectedHarvestDate;
+    try {
+      const res: any = await updateDataPertanian(params.id, data);
+      if (res) {
+        toast.success("Data updated successfully!", {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      } else {
+        toast.error("Data failed to update", {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      }
+    } catch {
+      toast.error("Data failed to update", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+  };
 
-		// 			<LocalizationProvider dateAdapter={AdapterDayjs}>
-		// 				<DemoContainer components={["DatePicker"]}>
-		// 					<DatePicker
-		// 						views={["year", "month", "day"]}
-		// 						format="DD-MM-YYYY"
-		// 						label="Tanggal Tanam"
-		// 					/>
-		// 				</DemoContainer>
-		// 			</LocalizationProvider>
+  return (
+    // <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+    // 	<Box
+    // 		sx={{
+    // 			bgcolor: "primary.main",
+    // 			height: 45,
+    // 			width: "100%",
+    // 			alignItems: "center",
+    // 			p: 0,
+    // 			boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    // 			zIndex: 2,
+    // 			position: "fixed",
+    // 		}}
+    // 	>
+    // 		<Button
+    // 			variant="text"
+    // 			startIcon={<ArrowBackIosNewIcon />}
+    // 			sx={{ bgcolor: "primary.main", color: "white" }}
+    // 			onClick={() => router.push("/home")}
+    // 		>
+    // 			Catat
+    // 		</Button>
+    // 	</Box>
 
-		// 			<LocalizationProvider dateAdapter={AdapterDayjs}>
-		// 				<DemoContainer components={["DatePicker"]}>
-		// 					<DatePicker
-		// 						views={["year", "month", "day"]}
-		// 						format="DD-MM-YYYY"
-		// 						label="Tanggal Panen"
-		// 					/>
-		// 				</DemoContainer>
-		// 			</LocalizationProvider>
+    // 	<Box
+    // 		sx={{
+    // 			mt: 8,
+    // 			width: 320,
+    // 			display: "flex",
+    // 			flexDirection: "column",
+    // 			alignItems: "center",
+    // 		}}
+    // 	>
+    // 		<Typography variant="h6" component="h6" sx={{ textAlign: "center", color: "primary.main" }}>
+    // 			Ubah Data Pertanian
+    // 		</Typography>
 
-		// 			<TextField sx={{ mt: 1 }} fullWidth label="Catatan" id="catatan" type="text" />
+    // 		<Box
+    // 			component="form"
+    // 			sx={{
+    // 				"& .MuiTextField-root": { width: 320, display: "flex", flexDirection: "column" },
+    // 			}}
+    // 			noValidate
+    // 			autoComplete="off"
+    // 		>
+    // 			<TextField required sx={{ mt: 3 }} fullWidth label="Produk" id="produk" type="text" />
 
-		// 			<TextField
-		// 				label="Jumlah Panen (kosongkan jika belum  panen)"
-		// 				id="jumlah panen"
-		// 				sx={{ mt: 1 }}
-		// 				InputProps={{
-		// 					endAdornment: <InputAdornment position="end">kg</InputAdornment>,
-		// 				}}
-		// 			/>
-		// 		</Box>
+    // 			<LocalizationProvider dateAdapter={AdapterDayjs}>
+    // 				<DemoContainer components={["DatePicker"]}>
+    // 					<DatePicker
+    // 						views={["year", "month", "day"]}
+    // 						format="DD-MM-YYYY"
+    // 						label="Tanggal Tanam"
+    // 					/>
+    // 				</DemoContainer>
+    // 			</LocalizationProvider>
 
-		// 		<Button fullWidth variant="contained" sx={{ mt: 2 }}>
-		// 			Simpan
-		// 		</Button>
+    // 			<LocalizationProvider dateAdapter={AdapterDayjs}>
+    // 				<DemoContainer components={["DatePicker"]}>
+    // 					<DatePicker
+    // 						views={["year", "month", "day"]}
+    // 						format="DD-MM-YYYY"
+    // 						label="Tanggal Panen"
+    // 					/>
+    // 				</DemoContainer>
+    // 			</LocalizationProvider>
 
-		// 		<Button fullWidth color="error" variant="outlined" sx={{ mt: 1 }}>
-		// 			Hapus Data
-		// 		</Button>
-		// 	</Box>
+    // 			<TextField sx={{ mt: 1 }} fullWidth label="Catatan" id="catatan" type="text" />
 
-		// 	<Shortcut />
-		// 	<Navigation />
-		// </Box>
+    // 			<TextField
+    // 				label="Jumlah Panen (kosongkan jika belum  panen)"
+    // 				id="jumlah panen"
+    // 				sx={{ mt: 1 }}
+    // 				InputProps={{
+    // 					endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+    // 				}}
+    // 			/>
+    // 		</Box>
 
-		<Stack
-			display={"flex"}
-			flexDirection={"column"}
-			alignItems={"center"}
-			sx={{ width: "100vw", height: "100vh" }}
-		>
-			{/* TOP BAR */}
-			<Paper
-				square
-				sx={{
-					bgcolor: "primary.main",
+    // 		<Button fullWidth variant="contained" sx={{ mt: 2 }}>
+    // 			Simpan
+    // 		</Button>
 
-					width: "100vw",
-					zIndex: 50,
-				}}
-			>
-				<Container maxWidth={"sm"} sx={{ display: "flex", justifyContent: "space-between" }}>
-					<Button
-						onClick={() => router.push("/")}
-						startIcon={<ArrowBackIosNewIcon />}
-						variant="text"
-						sx={{ color: "#ffffff" }}
-					>
-						Update Catat Pertanian
-					</Button>
-				</Container>
-			</Paper>
+    // 		<Button fullWidth color="error" variant="outlined" sx={{ mt: 1 }}>
+    // 			Hapus Data
+    // 		</Button>
+    // 	</Box>
 
-			{/* CONTENT */}
-			<Typography
-				variant="h6"
-				component="h6"
-				sx={{ textAlign: "center", color: "primary.main", my: 3 }}
-			>
-				Update Catat Data Pertanian
-			</Typography>
-			<Box
-				component="form"
-				sx={{
-					"& .MuiTextField-root": { width: "100%" },
-				}}
-				noValidate
-				autoComplete="off"
-				// onSubmit={handleSubmit}
-			>
-				<FormControl sx={{ my: 1 }} fullWidth>
-					<InputLabel id="produkSelected">Produk</InputLabel>
-					<Select
-						onChange={handleChange}
-						value={selectedProduct}
-						labelId="produkSelected"
-						label="Produk"
-					>
-						{products?.map((product) => (
-							<MenuItem key={product.id} value={product.name}>
-								{product.name}
-							</MenuItem>
-						))}
-					</Select>
-				</FormControl>
+    // 	<Shortcut />
+    // 	<Navigation />
+    // </Box>
 
-				<Stack sx={{ my: 1 }} direction={"row"} justifyContent={"space-between"} gap={2}>
-					<LocalizationProvider dateAdapter={AdapterDayjs}>
-						<DatePicker format="DD/MM/YYYY" label="Tanggal Tanam" />
-					</LocalizationProvider>
-					<Typography
-						variant="h6"
-						component="h6"
-						sx={{
-							display: "flex",
-							justifyContent: "center",
-							alignItems: "center",
-						}}
-					>
-						-
-					</Typography>
-					<LocalizationProvider dateAdapter={AdapterDayjs}>
-						<DatePicker format="DD/MM/YYYY" label="Tanngal Panen" />
-					</LocalizationProvider>
-				</Stack>
+    <Stack
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      sx={{ width: "100vw", height: "100vh" }}
+    >
+      {/* TOP BAR */}
+      <Paper
+        square
+        sx={{
+          bgcolor: "primary.main",
 
-				{/* <Typography>{formattedDatePlants}</Typography> */}
+          width: "100vw",
+          zIndex: 50,
+        }}
+      >
+        <Container
+          maxWidth={"sm"}
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Button
+            onClick={() => router.push("/")}
+            startIcon={<ArrowBackIosNewIcon />}
+            variant="text"
+            sx={{ color: "#ffffff" }}
+          >
+            Update Catat Pertanian
+          </Button>
+        </Container>
+      </Paper>
 
-				<TextField
-					sx={{ my: 1 }}
-					fullWidth
-					label="Catatan"
-					id="catatan"
-					type="text"
-					value={description}
-					onChange={(e) => setDescription(e.target.value)}
-				/>
-				{/* <TextField
+      <ToastContainer />
+
+      {/* CONTENT */}
+      <Typography
+        variant="h6"
+        component="h6"
+        sx={{ textAlign: "center", color: "primary.main", my: 3 }}
+      >
+        Update Catat Data Pertanian
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { width: "100%" },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <FormControl sx={{ my: 1 }} fullWidth>
+          <InputLabel id="produkSelected">Produk</InputLabel>
+          <Select
+            onChange={handleChange}
+            value={selectedProduct}
+            labelId="produkSelected"
+            label="Produk"
+            name="productId"
+          >
+            {products?.map((product) => (
+              <MenuItem key={product.id} value={product.id}>
+                {product.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <Stack
+          sx={{ my: 1 }}
+          direction={"row"}
+          justifyContent={"space-between"}
+          gap={2}
+        >
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              format="DD/MM/YYYY"
+              label="Tanggal Tanam"
+              value={selectedPlantingDate}
+              onChange={(date) => handlePlantingDateChange(date)}
+            />
+          </LocalizationProvider>
+          <Typography
+            variant="h6"
+            component="h6"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            -
+          </Typography>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              format="DD/MM/YYYY"
+              label="Tanggal Panen"
+              value={selectedHarvestDate}
+              onChange={(date) => handleHarvestDateChange(date)}
+            />
+          </LocalizationProvider>
+        </Stack>
+
+        {/* <Typography>{formattedDatePlants}</Typography> */}
+
+        <TextField
+          sx={{ my: 1 }}
+          fullWidth
+          label="Catatan"
+          id="catatan"
+          type="text"
+          value={description}
+          name="description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        {/* <TextField
 					sx={{ my: 1 }}
 					label="Jumlah Panen (kosongkan jika belum  panen)"
 					id="jumlah panen"
@@ -309,22 +399,23 @@ export default function UpdatePertanian({ params }: { params: { id: string } }) 
 					}}
 				/> */}
 
-				<TextField
-					sx={{ my: 1 }}
-					label="Jumlah Panen (kosongkan jika belum  panen)"
-					id="jumlah panen"
-					value={quantity}
-					InputProps={{
-						endAdornment: <InputAdornment position="end">kg</InputAdornment>,
-						inputMode: "numeric",
-					}}
-				/>
+        <TextField
+          sx={{ my: 1 }}
+          label="Jumlah Panen (kosongkan jika belum  panen)"
+          id="jumlah panen"
+          value={quantity}
+          name="quantity"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+            inputMode: "numeric",
+          }}
+        />
 
-				<Button type="submit" variant="contained" sx={{ width: "100%", my: 1 }}>
-					Simpan
-				</Button>
-			</Box>
-			<Navigation />
-		</Stack>
-	);
+        <Button type="submit" variant="contained" sx={{ width: "100%", my: 1 }}>
+          Simpan
+        </Button>
+      </Box>
+      <Navigation />
+    </Stack>
+  );
 }
--- services/transactions-service.tsx
+++ services/transactions-service.tsx
@@ -67,3 +67,50 @@ export const getListYields = async () => {
 
   return response;
 };
+
+export const updateDataPertanian = async (id: string, data: any) => {
+  let response = null;
+  let bearer = "";
+
+  if (typeof window !== "undefined") {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      bearer = `Bearer ${accessToken}`;
+
+      try {
+        const config = {
+          headers: {
+            Authorization: bearer,
+            "Content-Type": "application/json",
+          },
+        };
+
+        const updatedData = {
+          productId: data?.productId,
+          plantingTime: data?.plantingTime,
+          harvestTime: data?.harvestTime,
+          description: data?.description,
+          quantity: parseInt(data?.quantity),
+          imageUrl: data?.imageUrl,
+          isHarvested: data?.isHarvested,
+        };
+
+        const response = await axios.patch(
+          `${process.env.NEXT_PUBLIC_SERVICE_BASE}/yields/${id}`,
+          updatedData,
+          config
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Error:", error);
+        return null;
+      }
+    } else {
+      response = { error: "Access token not found" };
+    }
+  } else {
+    response = { error: "localStorage not available on the server" };
+  }
+
+  return response;
+};
